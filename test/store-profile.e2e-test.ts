fa61d4e68f40e0aa772172fa6e5289ad9162b5ee
4llves <jhonatab4@gmail.com>
2024-06-19 17:52:50 -0300

    test: implementing test e2e profile update

---
+++ test/store-profile.e2e-test.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@playwright/test'
+
+test('update profile successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill('4 Pizza')
+  await page.getByLabel('Descrição').fill('Another Description')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Perfil atualizado com sucesso')
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+
+  await page.waitForTimeout(250)
+
+  expect(page.getByRole('button', { name: '4 Pizza' })).toBeVisible()
+
+  // await page.waitForTimeout(2000)
+})
